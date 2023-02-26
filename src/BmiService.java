3940d844f580b1652985e486552b92970bcf2611
Andrey Fedorov <fvandre@outlook.com>
2023-02-27 02:09:04 +0300

    change code

--- src/BmiService.java
+++ src/BmiService.java
@@ -3,8 +3,7 @@ public class BmiService {
     public int calculate (double meter, double kilogram){
 
         double index = kilogram / (meter*meter);
-        int integerIndex = (int) index;
 
-        return integerIndex;
+        return (int) index;
     }
 }
